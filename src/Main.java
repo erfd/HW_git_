01c9ca9645c2a37f21b2f3acbd2d75b78064e23a
erfd <gnmbbb@gmail.com>
2013-11-17 01:48:08 +0400

    step hw4

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hw");
+        System.out.println("hw2");
+        System.out.println("3");
+        System.out.println("4");
     }
 }
